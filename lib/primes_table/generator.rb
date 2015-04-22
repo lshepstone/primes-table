75a2282c6dd31c7aa16c66c1aa1a2c789870d178
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-04-22 09:50:57 +0100

    Improves the generator, but only slightly

--- lib/primes_table/generator.rb
+++ lib/primes_table/generator.rb
@@ -1,15 +1,13 @@
 module PrimesTable
   class Generator
     def self.generate(size)
-      primes = []
-
-      number = 2
-      until primes.size == size
-        primes << number unless (2...number).any? { |i| number % i == 0 }
-        number += 1
+      [2].tap do |primes|
+        number = 3
+        until primes.size == size
+          primes << number unless (2...number).any? { |i| number % i == 0 }
+          number += 1
+        end
       end
-
-      primes
     end
   end
 end
