36c9bff57c05850a7b7d6cf683db39a8073dc7f4
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-04-22 09:36:26 +0100

    Use OptionParser to take care of parsing the count option

--- lib/primes_table/console.rb
+++ lib/primes_table/console.rb
@@ -1,12 +1,17 @@
+require 'optparse'
+
 module PrimesTable
   class Console
-    def initialize(arguments)
-      @arguments = arguments
+    def initialize
+      @options = {}
+      OptionParser.new do |opt|
+        opt.on('--count INTEGER') { |value| @options[:size] = value }
+      end.parse!
     end
 
     def print_primes_table
       puts "primes-table version #{PrimesTable::VERSION}\n\n"
-      Printer.new(Table.new(10)).print_table
+      Printer.new(Table.new(@options[:size].to_i)).print_table
     end
   end
 end
